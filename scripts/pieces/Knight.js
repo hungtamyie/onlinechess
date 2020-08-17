87ab165e4b1c1eb27e3b9d247ace17724d1ef847
Luke Henize <47581739+lhenize5@users.noreply.github.com>
2020-08-17 16:55:24 -0400

    added Knight spacesCovered method

--- scripts/pieces/Knight.js
+++ scripts/pieces/Knight.js
@@ -4,6 +4,31 @@ class Knight extends Piece {
         this.name = "knight";
     }
     spacesCovered() {
-        //returns array of spaces covered
+        //RETURNS ARRAY OF SPACES COVERED
+        function findKnightMoves(x, y) { //fills allKnightMoves array
+            if(x > -1 && x < 8 && y > -1 && y < 8) {
+                allKnightMoves.push(Array(x, y));
+            }
+        }
+        let availableSpaces = []; //array that will be returned
+        let allKnightMoves = []; //holds all 8 possible knight moves
+        let x = this.x; //current knight position
+        let y = this.y; //current knight position
+        findKnightMoves(x+1, y+2);
+        findKnightMoves(x+2, y+1);
+        findKnightMoves(x+2, y-1);
+        findKnightMoves(x+1, y-2);
+        findKnightMoves(x-1, y-2);
+        findKnightMoves(x-2, y-1);
+        findKnightMoves(x-2, y+1);
+        findKnightMoves(x-1, y+2);
+        //move legal spaces into availableSpaces array
+        allKnightMoves.forEach(
+            currentValue => { if(this.isOccupied(currentValue, this) == 0 || this.isOccupied(currentValue, this) == -1) {
+                availableSpaces.push(currentValue);
+                }
+            }
+        )
+        return availableSpaces;
     }
 }
