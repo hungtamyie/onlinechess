2d554295ee86847102114de73e9f7ec57bb146d5
Luke Henize <47581739+lhenize5@users.noreply.github.com>
2020-08-16 20:44:20 -0400

    moved piece map method to Piece class

--- scripts/pieces/Bishop.js
+++ scripts/pieces/Bishop.js
@@ -4,25 +4,6 @@ class Bishop extends Piece {
         this.name = "bishop";
     }
     spacesCovered() {
-        function isOccupied(space, piece) { //gets access to piece map
-            let pieceMap = piece.myGame.getMap();
-            if(pieceMap[space[1]][space[0]] == piece.team) {
-                //if pieceMap holds the team number at that spot, space is occupied by teammate
-                if((space[1] == piece.y) && (space[0] == piece.x)) {
-                    //check to see if "teammate" is really the current piece
-                    return 2;
-                } else {
-                    //if not us, then it's a different teammate
-                    return 1;
-                }
-            }else if((pieceMap[space[1]][space[0]] != piece.team) && (pieceMap[space[1]][space[0]] != 0)) {
-                //if space is occupied by enemy
-                return 0;
-            }else {
-                return -1;
-            }
-        }
-
         //RETURNS ARRAY OF SPACES COVERED
         let availableSpaces = []; //array that will be returned
         let currentSpace = []; //array to temporarily hold spaces
@@ -34,7 +15,7 @@ class Bishop extends Piece {
             while(x > -1 && x < 8 && y > -1 && y < 8) { //while bishop is on board
             currentSpace.push(x);
             currentSpace.push(y);
-            if(isOccupied(currentSpace, this) == 2) {
+            if(this.isOccupied(currentSpace, this) == 2) {
                 //if space is occupied by current piece, don't include
                 if(i==0) {
                     x++;
@@ -49,10 +30,10 @@ class Bishop extends Piece {
                     x--;
                     y++;
                 }
-            }else if(isOccupied(currentSpace, this) == 1) {
+            }else if(this.isOccupied(currentSpace, this) == 1) {
                 //if space is occupied by team, don't include and stop searching
                 x = 8;
-            }else if(isOccupied(currentSpace, this) == 0) {
+            }else if(this.isOccupied(currentSpace, this) == 0) {
                 //if space is occupied by enemy, include and stop searching
                 availableSpaces.push(currentSpace);
                 y = 8;
--- scripts/pieces/Rook.js
+++ scripts/pieces/Rook.js
@@ -4,25 +4,6 @@ class Rook extends Piece {
         this.name = "rook";
     }
     spacesCovered() {
-        function isOccupied(space, piece) { //gets access to piece map
-            let pieceMap = piece.myGame.getMap();
-            if(pieceMap[space[1]][space[0]] == piece.team) {
-                //if pieceMap holds the team number at that spot, space is occupied by teammate
-                if((space[1] == piece.y) && (space[0] == piece.x)) {
-                    //check to see if "teammate" is really the current piece
-                    return 2;
-                } else {
-                    //if not us, then it's a different teammate
-                    return 1;
-                }
-            }else if((pieceMap[space[1]][space[0]] != piece.team) && (pieceMap[space[1]][space[0]] != 0)) {
-                //if space is occupied by enemy
-                return 0;
-            }else {
-                return -1;
-            }
-        }
-
         //RETURNS ARRAY OF SPACES COVERED
         let availableSpaces = []; //array that will be returned
         let currentSpace = []; //array to temporarily hold spaces
@@ -31,13 +12,13 @@ class Rook extends Piece {
         while(y < 8) { //finding available spaces UPWARD
             currentSpace.push(this.x); //find x coord
             currentSpace.push(y); //then find y coord of space
-            if(isOccupied(currentSpace, this) == 2) {
+            if(this.isOccupied(currentSpace, this) == 2) {
                 //if space is occupied by current piece, don't include
                 y++;
-            }else if(isOccupied(currentSpace, this) == 1) {
+            }else if(this.isOccupied(currentSpace, this) == 1) {
                 //if space is occupied by team, don't include and stop searching
                 y = 8;
-            }else if(isOccupied(currentSpace, this) == 0) {
+            }else if(this.isOccupied(currentSpace, this) == 0) {
                 //if space is occupied by enemy, include and stop searching
                 availableSpaces.push(currentSpace);
                 y = 8;
@@ -53,13 +34,13 @@ class Rook extends Piece {
         while(y > -1) { //finding available spaces DOWNWARD
             currentSpace.push(this.x); //find x coord
             currentSpace.push(y); //then find y coord of space
-            if(isOccupied(currentSpace, this) == 2) {
+            if(this.isOccupied(currentSpace, this) == 2) {
                 //if space is occupied by current piece, don't include
                 y--;
-            }else if(isOccupied(currentSpace, this) == 1) {
+            }else if(this.isOccupied(currentSpace, this) == 1) {
                 //if space is occupied by team, don't include and stop searching
                 y = -1;
-            }else if(isOccupied(currentSpace, this) == 0) {
+            }else if(this.isOccupied(currentSpace, this) == 0) {
                 //if space is occupied by enemy, include and stop searching
                 availableSpaces.push(currentSpace);
                 y = -1;
@@ -75,13 +56,13 @@ class Rook extends Piece {
         while(x > -1) { //finding available spaces TO THE LEFT
             currentSpace.push(x); //find x coord
             currentSpace.push(this.y); //then find y coord
-            if(isOccupied(currentSpace, this) == 2) {
+            if(this.isOccupied(currentSpace, this) == 2) {
                 //if space is occupied by current piece, don't include
                 x--;
-            }else if(isOccupied(currentSpace, this) == 1) {
+            }else if(this.isOccupied(currentSpace, this) == 1) {
                 //if space is occupied by team, don't include and stop searching
                 x = -1;
-            }else if(isOccupied(currentSpace, this) == 0) {
+            }else if(this.isOccupied(currentSpace, this) == 0) {
                 //if space is occupied by enemy, include and stop searching
                 availableSpaces.push(currentSpace);
                 x = -1;
@@ -97,13 +78,13 @@ class Rook extends Piece {
         while(x < 8) { //finding available spaces TO THE RIGHT
             currentSpace.push(x); //find x coord
             currentSpace.push(this.y); //then find y coord
-            if(isOccupied(currentSpace, this) == 2) {
+            if(this.isOccupied(currentSpace, this) == 2) {
                 //if space is occupied by current piece, don't include
                 x++;
-            }else if(isOccupied(currentSpace, this) == 1) {
+            }else if(this.isOccupied(currentSpace, this) == 1) {
                 //if space is occupied by team, don't include and stop searching
                 x = 8;
-            }else if(isOccupied(currentSpace, this) == 0) {
+            }else if(this.isOccupied(currentSpace, this) == 0) {
                 //if space is occupied by enemy, include and stop searching
                 availableSpaces.push(currentSpace);
                 x = 8;
