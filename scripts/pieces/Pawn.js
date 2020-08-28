bc74b6d35aa6fc9f9acc7037a8a65e2cd6614ef6
Luke Henize <47581739+lhenize5@users.noreply.github.com>
2020-08-27 20:55:51 -0400

    fixed pawns, temporarily removed en passant

--- scripts/pieces/Pawn.js
+++ scripts/pieces/Pawn.js
@@ -21,7 +21,7 @@ class Pawn extends Piece {
                 findPawnMoves(x, y+2);
             }
             findPawnMoves(x-1, y+1);
-            findPawnMoves(x+1, y+1);
+            findPawnMoves(x+1, y+1); //diagonal spaces
         }else if(this.team == 2) { //if pawn is black
             findPawnMoves(x, y-1);
             if(this.moveCount == 0) { //if pawn hasn't moved, it can move forward twice
@@ -29,42 +29,24 @@ class Pawn extends Piece {
             }
             findPawnMoves(x-1, y-1);
             findPawnMoves(x+1, y-1); //diagonal spaces
-            findPawnMoves(x-1, y);
-            findPawnMoves(x+1, y); //en passant spaces
         }
         
 
         //move legal spaces into availableSpaces array
         allPawnMoves.forEach(
-            (currentValue, index) => { 
-                if(index == 0) { //if we're checking straight forward space
+            (currentValue) => { 
+                if(currentValue[0] == x && ((currentValue[1] == (y+1)) || (currentValue[1] == (y-1)))) { //if currentValue is 1 space forward
                     //make sure it's unoccupied
                     if(this.isOccupied(currentValue, this) == -1) {
                         availableSpaces.push(currentValue);
                     }
-                }else if(allPawnMoves.length == 6 && index == 1) { //if currentValue is 2 spaces forward
+                }else if((currentValue[1] == (y+2)) || (currentValue[1] == (y-2))) { //if currentValue is 2 spaces forward
                     if(this.isOccupied(currentValue, this) == -1) {
                         availableSpaces.push(currentValue);
                     }
-                }else if(index == (allPawnMoves.length - 2)) { //left en passant
-                    if(this.isOccupied(currentValue, this) == -1) {
-                        if(this.myGame.pieceAt(currentValue[0], currentValue[1]).moveCount == 1) { //if we can take en passant
-                            currentValue[0]--;
-                            currentValue[1]++; //make sure we push the space behind enemy pawn
-                            availableSpaces.push(currentValue);
-                        }
-                    }
-                }else if(index == (allPawnMoves.length - 1)) { //rigth en passant
-                    if(this.isOccupied(currentValue, this) == -1) {
-                        if(this.myGame.pieceAt(currentValue[0], currentValue[1]).moveCount == 1) { //if we can take en passant
-                            currentValue[0]++;
-                            currentValue[1]++; //make sure we push the space behind enemy pawn
-                            availableSpaces.push(currentValue);
-                        }
-                    }
-                }else { //check diagonal spaces
+                }else if((currentValue[0] == (x-1)) || (currentValue[0] == (x+1))) { //check diagonal spaces
                     if(this.isOccupied(currentValue, this) == 0) { //if occupied by enemy
-                    availableSpaces.push(currentValue);
+                        availableSpaces.push(currentValue);
                     }
                 }
             }
